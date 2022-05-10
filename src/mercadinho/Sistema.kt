78095be079bc48c553c14b004a279324ee0630c7
joanascheer <scheer.joana@gmail.com>
2022-05-10 09:44:48 -0300

    Organizando

--- src/mercadinho/Sistema.kt
+++ src/mercadinho/Sistema.kt
@@ -1,5 +1,7 @@
 package mercadinho
 
+import kotlin.system.exitProcess
+
 class Sistema {
     val listaDeVerdura: MutableList<Verduras> = ArrayList()
     val listaDeLegume: MutableList<Legumes> = ArrayList()
@@ -42,8 +44,18 @@ class Sistema {
                         listaOutros.add(outro)
                     }
                     "5" -> {
-                        //Verduras().mostraListaVerdura(listaDeVerdura) -> só nao pega quantidade
+                        println("-----------------------------\n" +
+                                "*** Sua lista de produtos ***\n" +
+                                "-----------------------------\n")
+
                         mostraLista(listaDeVerdura, listaDeGrao, listaDeLegume, listaOutros)
+
+                        println("          t o t a l           \n")
+
+                        mostraindices(listaDeVerdura, listaDeGrao, listaDeLegume, listaOutros)
+                        sair()
+
+
                     }
                     else -> throw IllegalArgumentException()
 
@@ -83,6 +95,30 @@ class Sistema {
         }
     }
 
+    fun mostraindices(
+        listaDeVerdura: MutableList<Verduras>,
+        listaDeGrao: MutableList<Graos>,
+        listaDeLegume: MutableList<Legumes>,
+        listaOutros: MutableList<Outros>,
+    ) {
+        for (i in listaDeVerdura.indices) {
+            println("A quantidade de verduras é ${listaDeVerdura.size}")
+        }
+
+        for (i in listaDeGrao.indices) {
+            println("A quantidade de grãos é ${listaDeGrao.size}")
+        }
+
+        for (i in listaDeLegume.indices) {
+            println("A quantidade de legumes é ${listaDeLegume.size}")
+        }
+
+        for (i in listaOutros.indices) {
+            println("A quantidade de outros produtos comprados é ${listaOutros.size}")
+        }
+
+    }
+
 //    fun insereProdutoLista() : ArrayList<Produto> {
 //        val listaDeProduto: ArrayList<Produto> = ArrayList()
 //
@@ -92,6 +128,9 @@ class Sistema {
 
     companion object Utilities {
 
-
+        fun sair() {
+            println("Obrigado(a) por comprar conosco!\n")
+            exitProcess(0)
+        }
     }
 }
